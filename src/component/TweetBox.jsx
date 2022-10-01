ad43f41fd4ac4012d76165ef711c1d0c811d9e67
Brandon <bliberto@uci.edu>
2022-10-01 16:05:58 -0700

    Add Post/Tweets to database and live rendering

--- src/component/TweetBox.jsx
+++ src/component/TweetBox.jsx
@@ -1,24 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 import "./TweetBox.css";
+import { db } from "../firebase/init";
+import { collection, addDoc } from "firebase/firestore";
 
 // ICONS
 import Button from "@mui/material/Button";
 import Avatar from "@mui/material/Avatar";
 
 export default function TweetBox() {
+  const [tweetMessage, setTweetMessage] = useState("");
+  const [tweetImage, setTweetImage] = useState("");
+
+  // GOOD HOOK TO USE
+  const {
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const sendTweet = (e) => {
+    e.preventDefault();
+    const post = {
+      displayName: "Brandon Libertore",
+      userName: "brandonlibertore",
+      verified: true,
+      text: tweetMessage,
+      image: tweetImage,
+      avatar:
+        "https://pbs.twimg.com/profile_images/1566451434597494785/vkiiW9pv_400x400.jpg",
+    };
+    addDoc(collection(db, "posts"), post);
+    setTweetMessage("");
+    setTweetImage("");
+  };
   return (
     <div className="tweet__box">
-      <form action="">
+      <form>
         <div className="tweet__box--input-text">
-          <Avatar src="" />
-          <input type="text" placeholder="What's Happening?" />
+          <Avatar src="https://pbs.twimg.com/profile_images/1566451434597494785/vkiiW9pv_400x400.jpg" />
+          <input
+            value={tweetMessage}
+            type="text"
+            placeholder="What's Happening?"
+            onChange={(e) => setTweetMessage(e.target.value)}
+          />
         </div>
         <input
+          value={tweetImage}
           className="tweet__box--input-img"
           type="text"
           placeholder="Optional: Enter Image URL"
+          onChange={(e) => setTweetImage(e.target.value)}
         />
-        <Button className="tweet__box--btn">Tweet</Button>
+        <Button onClick={sendTweet} className="tweet__box--btn">
+          Tweet
+        </Button>
       </form>
     </div>
   );
