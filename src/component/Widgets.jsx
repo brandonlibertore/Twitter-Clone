8941ed5b89e46f24409e8efd77eab3876a1b710f
Brandon <bliberto@uci.edu>
2022-10-01 14:57:30 -0700

    Widgets Created (Right Sidebar)

--- src/component/Widgets.jsx
+++ src/component/Widgets.jsx
@@ -1,10 +1,36 @@
 import React from "react";
 import "./Widgets.css";
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterTweetEmbed,
+} from "react-twitter-embed";
+
+// ICONS
+import SearchIcon from "@mui/icons-material/Search";
 
 export default function Widgets() {
   return (
     <div className="widgets">
-      <h1>Widgets</h1>
+      <div className="widgets__input">
+        <SearchIcon className="widgets__search--icon" />
+        <input type="text" placeholder="Search Twitter" />
+      </div>
+      <div className="widgets__widget--container">
+        <h2>What's Happening?</h2>
+        <TwitterTweetEmbed tweetId={"858551177860055040"} />
+
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="cleverqazi"
+          options={{ height: 400 }}
+        />
+
+        <TwitterShareButton
+          url={"https:/facebook.com/cleverprogrammer"}
+          options={{ text: "#reactjs is awesome", via: "cleverqazi" }}
+        />
+      </div>
     </div>
   );
 }
